8b28e76f8a254b8d5519d32315cfa184f5de1438
CreeperCodeDev <creeperdev@hotmail.com>
2025-03-09 23:56:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const text = "我們是一間便宜低延遲的伺服器托管";
 let index = 0;
+
 function typeEffect() {
     if (index < text.length) {
         document.getElementById("typing-text").textContent += text.charAt(index);
         index++;
         setTimeout(typeEffect, 100);
+    } else {
+        document.getElementById("typing-text").style.borderRight = "none";
     }
 }
+
 window.onload = typeEffect;
